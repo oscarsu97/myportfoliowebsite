bf58124aaf1a58a2eda09339213ab7be84b2aebb
oscarsu97 <zhenhuanghuang97@gmail.com>
2020-08-10 23:05:09 +0800

    Updates

---
+++ precache-manifest.d9001947633cfe102bb4f544a09509d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e09d85b65844918446f7b250341b39d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0916633c45d32ec49675",
+    "url": "/static/css/2.def00553.chunk.css"
+  },
+  {
+    "revision": "5f0c90b853408f96caa0",
+    "url": "/static/css/main.c6781dfd.chunk.css"
+  },
+  {
+    "revision": "0916633c45d32ec49675",
+    "url": "/static/js/2.c4c50501.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.c4c50501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0c90b853408f96caa0",
+    "url": "/static/js/main.c683d902.chunk.js"
+  },
+  {
+    "revision": "4db15eb8b2236008fc5a",
+    "url": "/static/js/runtime-main.d69be0a5.js"
+  },
+  {
+    "revision": "0d8863377df58c177c26f5d9c088a88d",
+    "url": "/static/media/profile_photo_cny.0d886337.jpg"
+  },
+  {
+    "revision": "31dff10e0b40fb5f324e1d4d50700735",
+    "url": "/static/media/profile_photo_japan.31dff10e.jpg"
+  }
+]);
